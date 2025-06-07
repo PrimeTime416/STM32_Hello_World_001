b8a85f9b27d5ff510810781a2d1938c2800d0886
PrimeTime416 <a2kerr@gmail.com>
2025-06-07 12:01:09 -0400

    fixes for the bare metal tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![allow(clippy::empty_loop)]
+
+use core::panic::PanicInfo;
+
+#[unsafe(export_name = "main")]
+pub extern "C" fn main() -> ! {
+    loop {
+        
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        
+    }
 }
