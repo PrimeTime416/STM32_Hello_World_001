f9b0af14e8aa1232fd1df8ca2926bb3205f8980a
PrimeTime416 <a2kerr@gmail.com>
2025-06-10 21:11:14 -0400

    svd file setup

--- src/startup_stm32f405.rs
+++ src/startup_stm32f405.rs
@@ -1,5 +1,11 @@
-// 1. Define the vectort table for the mcu
+// 1. Define the vector table for the mcu
+static VECTOR_TABLE: [Option<unsafe fn()>; 95] = [
+    Some(reset_handler()),
+    Some(nmi_handler()),
+    Some(hardfault_handler()),
 
+
+];
 // 2. Define the reset handler
 #[unsafe(no_mangle)]
 fn reset_handler(){
@@ -7,3 +13,23 @@ fn reset_handler(){
 }
 
 // 3. Define the exception handler 
+#[unsafe(no_mangle)]
+fn nmi_handler(){
+    loop {
+        
+    }
+}
+
+#[unsafe(no_mangle)]
+fn default_handler(){
+    loop {
+        
+    }
+}
+
+#[unsafe(no_mangle)]
+fn hardfault_handler(){
+    loop {
+        
+    }
+}
