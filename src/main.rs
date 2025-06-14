40271711cae9ad5a08230cb8eb830ca3eb21edd3
PrimeTime416 <a2kerr@gmail.com>
2025-06-14 12:27:01 -0400

    troubleshooting reset vector

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,16 @@
 #![allow(clippy::empty_loop)]
 
 use core::panic::PanicInfo;
-
 mod startup_stm32f405;
-
-static mut SCORES_GLOBAL: [i32; 5] = [1, 2, 3, 4, 5];
-const NUMBERS: [i32; 5] = [1, 2, 3, 4, 5];
-static mut BUFFER: [u8; 1024] = [0; 1024];
-
 // Placed into CCMRAM for fast access
-#[unsafe(link_section = ".ccmram")]
-static mut CCM_BUFFER: [u8; 512] = [0; 512];
+// #[unsafe(link_section = ".ccmram")]
+// static mut CCM_BUFFER: [u8; 512] = [0; 512];
 
 // Stored in backup SRAM so it survives standby mode
-#[unsafe(link_section = ".bkpsram")]
-static mut BKP_DATA: [u32; 8] = [0; 8];
 
-#[unsafe(export_name = "main")]
+// #[unsafe(export_name = "main")]
+#[unsafe(no_mangle)]
 pub extern "C" fn main() -> ! {
-    let mut _total_score: i32 = 0;
-    for score in unsafe { SCORES_GLOBAL } {
-        _total_score += score;
-    };
-
-    for score in NUMBERS {
-    _total_score += score;
-}
-
-    unsafe {
-        BUFFER[0] = 100;
-        CCM_BUFFER[0] = 1;
-        BKP_DATA[0] = 42;
-    }
     loop {
         
     }
--- src/startup_stm32f405.rs
+++ src/startup_stm32f405.rs
@@ -1,289 +1,304 @@
 /*
-When writing embedded Rust for ARM Cortex-M MCUs (e.g., STM32F405), 
-naming interrupt and exception handlers must conform to hardware expectations and C toolchain conventions, 
-not Rust's snake_case style. The suffix Handler is capitalized (PascalCase) by long-standing C/assembly convention.
+Startup code for STM32F405 in embedded Rust. 
+- Defines the interrupt vector table
+- Implements the Reset handler
+- Provides default implementations for all other exception/interrupt handlers
 */
 
 use core::ptr;
 
 unsafe extern "C" {
-    static _sidata: u32 ; /* Start of .data section in flash*/
-    static mut _sdata: u32 ; /* Start of . data section in RAM */
-    static mut _edata: u32 ; /* End of .data section in RAM */
-    static mut _sbss: u32 ; /*Start of .bss in RAM */
-    static mut _ebss: u32 ; /* End of .bss in RAM */
+    unsafe static _estack: u32;           // End of RAM, used to initialize MSP
+    unsafe static _sidata: u32;           // Start of .data section in flash
+    unsafe static mut _sdata: u32;        // Start of .data section in RAM
+    unsafe static mut _edata: u32;        // End of .data section in RAM
+    unsafe static mut _sbss: u32;         // Start of .bss section in RAM
+    unsafe static mut _ebss: u32;         // End of .bss section in RAM
 }
 
+const NONE: VectorEntry = VectorEntry { pointer: 0 as *const u32 };
 
 
-// 1. Define the vector table for the mcu
-#[used]
-#[allow(non_snake_case)]
+#[repr(C)]
+pub union VectorEntry {
+    pub handler: unsafe extern "C" fn(),
+    pub pointer: *const u32,
+}
+
+unsafe impl Sync for VectorEntry {}
+#[unsafe (link_section = ".isr_vector")]
 #[unsafe(no_mangle)]
-#[unsafe(link_section = ".isr_vector")]
-static VECTOR_TABLE: [Option< unsafe extern "C" fn()>; 97] = [
-    Some(Reset_Handler),
-    Some(NMI_Handler),
-    Some(HardFault_Handler),
-    Some(MemManage_Handler),
-    Some(BusFault_Handler),
-    Some(UsageFault_Handler),
-    Some(Reserved_Handler),
-    Some(Reserved_Handler),
-    Some(Reserved_Handler),
-    Some(Reserved_Handler),
-    Some(SVCall_Handler),
-    Some(Debug_Monitor_Handler),
-    Some(Reserved_Handler),
-    Some(PendSV_Handler),
-    Some(SysTick_Handler),
-    Some(WWDG_Handler),
-    Some(PVD_Handler),
-    Some(TAMP_STAMP_Handler),
-    Some(RTC_WKUP_Handler),
-    Some(FLASH_Handler),
-    Some(RCC_Handler),
-    Some(EXTI0_Handler),
-    Some(EXTI1_Handler),
-    Some(EXTI2_Handler),
-    Some(EXTI3_Handler),
-    Some(EXTI4_Handler),
-    Some(DMA1_Stream0_Handler),
-    Some(DMA1_Stream1_Handler),
-    Some(DMA1_Stream2_Handler),
-    Some(DMA1_Stream3_Handler),
-    Some(DMA1_Stream4_Handler),
-    Some(DMA1_Stream5_Handler),
-    Some(DMA1_Stream6_Handler),
-    Some(ADC_Handler),
-    Some(CAN1_TX_Handler),
-    Some(CAN1_RX0_Handler),
-    Some(CAN1_RX1_Handler),
-    Some(CAN1_SCE_Handler),
-    Some(EXTI9_5_Handler),
-    Some(TIM1_BRK_TIM9_Handler),
-    Some(TIM1_UP_TIM10_Handler),
-    Some(TIM1_TRG_COM_TIM11_Handler),
-    Some(TIM1_CC_Handler),
-    Some(TIM2_Handler),
-    Some(TIM3_Handler),
-    Some(TIM4_Handler),
-    Some(I2C1_EV_Handler),
-    Some(I2C1_ER_Handler),
-    Some(I2C2_EV_Handler),
-    Some(I2C2_ER_Handler),
-    Some(SPI1_Handler),
-    Some(SPI2_Handler),
-    Some(USART1_Handler),
-    Some(USART2_Handler),
-    Some(USART3_Handler),
-    Some(EXTI15_10_Handler),
-    Some(RTC_Alarm_Handler),
-    Some(OTG_FS_WKUP_Handler),
-    Some(TIM8_BRK_TIM12_Handler),
-    Some(TIM8_UP_TIM13_Handler),
-    Some(TIM8_TRG_COM_TIM14_Handler),
-    Some(TIM8_CC_Handler),
-    Some(DMA1_Stream7_Handler),
-    Some(FSMC_Handler),
-    Some(SDIO_Handler),
-    Some(TIM5_Handler),
-    Some(SPI3_Handler),
-    Some(UART4_Handler),
-    Some(UART5_Handler),
-    Some(TIM6_DAC_Handler),
-    Some(TIM7_Handler),
-    Some(DMA2_Stream0_Handler),
-    Some(DMA2_Stream1_Handler),
-    Some(DMA2_Stream2_Handler),
-    Some(DMA2_Stream3_Handler),
-    Some(DMA2_Stream4_Handler),
-    Some(ETH_Handler),
-    Some(ETH_WKUP_Handler),
-    Some(CAN2_TX_Handler),
-    Some(CAN2_RX0_Handler),
-    Some(CAN2_RX1_Handler),
-    Some(CAN2_SCE_Handler),
-    Some(OTG_FS_Handler),
-    Some(DMA2_Stream5_Handler),
-    Some(DMA2_Stream6_Handler),
-    Some(DMA2_Stream7_Handler),
-    Some(USART6_Handler),
-    Some(I2C3_EV_Handler),
-    Some(I2C3_ER_Handler),
-    Some(OTG_HS_EP1_OUT_Handler),
-    Some(OTG_HS_EP1_IN_Handler),
-    Some(OTG_HS_WKUP_Handler),
-    Some(OTG_HS_Handler),
-    Some(DCMI_Handler),
-    Some(CRYP_Handler),
-    Some(HASH_RNG_Handler),
-    Some(FPU_Handler),
+#[used]
+pub static VECTOR_TABLE: [VectorEntry; 98] = [
+    VectorEntry { pointer: unsafe { &_estack } },  // Initial Stack Pointer
+    VectorEntry { handler: Reset_Handler },
+    VectorEntry { handler: NMI_Handler },
+    VectorEntry { handler: HardFault_Handler },
+    VectorEntry { handler: MemManage_Handler },
+    VectorEntry { handler: BusFault_Handler },
+    VectorEntry { handler: UsageFault_Handler },
+    NONE,
+    NONE,
+    NONE,
+    NONE,
+    VectorEntry { handler: SVCall_Handler },
+    VectorEntry { handler: Debug_Monitor_Handler },
+    NONE,
+    VectorEntry { handler: PendSV_Handler },
+    VectorEntry { handler: SysTick_Handler },
+    VectorEntry { handler: WWDG_Handler },
+    VectorEntry { handler: PVD_Handler },
+    VectorEntry { handler: TAMP_STAMP_Handler },
+    VectorEntry { handler: RTC_WKUP_Handler },
+    VectorEntry { handler: FLASH_Handler },
+    VectorEntry { handler: RCC_Handler },
+    VectorEntry { handler: EXTI0_Handler },
+    VectorEntry { handler: EXTI1_Handler },
+    VectorEntry { handler: EXTI2_Handler },
+    VectorEntry { handler: EXTI3_Handler },
+    VectorEntry { handler: EXTI4_Handler },
+    VectorEntry { handler: DMA1_Stream0_Handler },
+    VectorEntry { handler: DMA1_Stream1_Handler },
+    VectorEntry { handler: DMA1_Stream2_Handler },
+    VectorEntry { handler: DMA1_Stream3_Handler },
+    VectorEntry { handler: DMA1_Stream4_Handler },
+    VectorEntry { handler: DMA1_Stream5_Handler },
+    VectorEntry { handler: DMA1_Stream6_Handler },
+    VectorEntry { handler: ADC_Handler },
+    VectorEntry { handler: CAN1_TX_Handler },
+    VectorEntry { handler: CAN1_RX0_Handler },
+    VectorEntry { handler: CAN1_RX1_Handler },
+    VectorEntry { handler: CAN1_SCE_Handler },
+    VectorEntry { handler: EXTI9_5_Handler },
+    VectorEntry { handler: TIM1_BRK_TIM9_Handler },
+    VectorEntry { handler: TIM1_UP_TIM10_Handler },
+    VectorEntry { handler: TIM1_TRG_COM_TIM11_Handler },
+    VectorEntry { handler: TIM1_CC_Handler },
+    VectorEntry { handler: TIM2_Handler },
+    VectorEntry { handler: TIM3_Handler },
+    VectorEntry { handler: TIM4_Handler },
+    VectorEntry { handler: I2C1_EV_Handler },
+    VectorEntry { handler: I2C1_ER_Handler },
+    VectorEntry { handler: I2C2_EV_Handler },
+    VectorEntry { handler: I2C2_ER_Handler },
+    VectorEntry { handler: SPI1_Handler },
+    VectorEntry { handler: SPI2_Handler },
+    VectorEntry { handler: USART1_Handler },
+    VectorEntry { handler: USART2_Handler },
+    VectorEntry { handler: USART3_Handler },
+    VectorEntry { handler: EXTI15_10_Handler },
+    VectorEntry { handler: RTC_Alarm_Handler },
+    VectorEntry { handler: OTG_FS_WKUP_Handler },
+    VectorEntry { handler: TIM8_BRK_TIM12_Handler },
+    VectorEntry { handler: TIM8_UP_TIM13_Handler },
+    VectorEntry { handler: TIM8_TRG_COM_TIM14_Handler },
+    VectorEntry { handler: TIM8_CC_Handler },
+    VectorEntry { handler: DMA1_Stream7_Handler },
+    VectorEntry { handler: FSMC_Handler },
+    VectorEntry { handler: SDIO_Handler },
+    VectorEntry { handler: TIM5_Handler },
+    VectorEntry { handler: SPI3_Handler },
+    VectorEntry { handler: UART4_Handler },
+    VectorEntry { handler: UART5_Handler },
+    VectorEntry { handler: TIM6_DAC_Handler },
+    VectorEntry { handler: TIM7_Handler },
+    VectorEntry { handler: DMA2_Stream0_Handler },
+    VectorEntry { handler: DMA2_Stream1_Handler },
+    VectorEntry { handler: DMA2_Stream2_Handler },
+    VectorEntry { handler: DMA2_Stream3_Handler },
+    VectorEntry { handler: DMA2_Stream4_Handler },
+    VectorEntry { handler: ETH_Handler },
+    VectorEntry { handler: ETH_WKUP_Handler },
+    VectorEntry { handler: CAN2_TX_Handler },
+    VectorEntry { handler: CAN2_RX0_Handler },
+    VectorEntry { handler: CAN2_RX1_Handler },
+    VectorEntry { handler: CAN2_SCE_Handler },
+    VectorEntry { handler: OTG_FS_Handler },
+    VectorEntry { handler: DMA2_Stream5_Handler },
+    VectorEntry { handler: DMA2_Stream6_Handler },
+    VectorEntry { handler: DMA2_Stream7_Handler },
+    VectorEntry { handler: USART6_Handler },
+    VectorEntry { handler: I2C3_EV_Handler },
+    VectorEntry { handler: I2C3_ER_Handler },
+    VectorEntry { handler: OTG_HS_EP1_OUT_Handler },
+    VectorEntry { handler: OTG_HS_EP1_IN_Handler },
+    VectorEntry { handler: OTG_HS_WKUP_Handler },
+    VectorEntry { handler: OTG_HS_Handler },
+    VectorEntry { handler: DCMI_Handler },
+    VectorEntry { handler: CRYP_Handler },
+    VectorEntry { handler: HASH_RNG_Handler },
+    VectorEntry { handler: FPU_Handler },
 ];
 
-// 2. Define the reset handler
-#[allow(non_snake_case)]
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn Reset_Handler(){
-    unsafe {
-        // 1. Copy the .data section from FLASH to RAM
-        // Reference of static variable to C like raw pointer
-        let mut src_is_flash = ptr::addr_of!(_sidata);
-        let mut dest_is_ram = ptr::addr_of_mut!(_sdata);
-        let data_end_in_ram = ptr::addr_of_mut!(_edata);
-
-        while dest_is_ram < data_end_in_ram {
-            *dest_is_ram = *src_is_flash;
-            dest_is_ram = dest_is_ram.add(1);
-            src_is_flash = src_is_flash.add(1);
+pub unsafe extern "C" fn Reset_Handler() {
+    // Copy .data from flash to RAM
+    let mut src = &_sidata as *const u32;
+    let mut dest = &raw mut _sdata as *mut u32;
+    while dest < &raw mut _edata as *mut u32 {
+        unsafe {
+        *dest = *src;
+        dest = dest.add(1);
+        src = src.add(1);
         }
+    }
 
-        // 2. Zero out the .bss section in the RAM
-        let mut bss = ptr::addr_of_mut!(_sbss);
-        let bss_end = ptr::addr_of_mut!(_ebss);
-
-        while bss < bss_end {
+    // Zero out .bss
+    let mut bss = &raw mut _sbss as *mut u32;
+    while bss < &raw mut _ebss as *mut u32 {
+        unsafe {
+            
             *bss = 0;
             bss = bss.add(1);
         }
     }
-    // 3.  call main
-    crate::main()
+
+    // Call main
+    crate::main();
 }
 
-// 3. Define the exception handler
-#[allow(non_snake_case)] 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn NMI_Handler(){
+pub unsafe extern "C" fn Default_Handler() {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn Reserved_Handler() {
+    unsafe { Default_Handler() }
+}
+
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn HardFault_Handler() {
+    unsafe extern "C" {
+        unsafe static mut _estack: u32;
+    }
+
     loop {
         
     }
+
+    let sp: *const u32;
+    core::arch::asm!("mrs {}, msp", out(reg) sp);
+
+    // Read stacked registers from PSP/MSP
+    let r0 = unsafe {*sp.offset(0)};
+    let r1 = *sp.offset(1);
+    let r2 = *sp.offset(2);
+    let r3 = *sp.offset(3);
+    let r12 = *sp.offset(4);
+    let lr = *sp.offset(5);
+    let pc = *sp.offset(6);
+    let psr = *sp.offset(7);
+
+    // core::asm::bkpt(); // so you can now inspect pc, lr, etc.
+    loop {}
 }
 
-#[allow(non_snake_case)]
-#[unsafe(no_mangle)]
-pub extern "C" fn HardFault_Handler(){
-    loop {
-        
-    }
-}
 
-#[allow(non_snake_case)]
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn Default_Handler(){
-    loop {
-        
-    }
-}
 
-// 4. Reserved handler for vector table
-#[allow(non_snake_case)]
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn Reserved_Handler(){
-    unsafe {Default_Handler()};
-}
-
-// Default handler Macro
 macro_rules! stub_handlers {
     ($($name:ident),*) => {
         $(
             #[unsafe(no_mangle)]
             pub unsafe extern "C" fn $name() {
-                unsafe {Default_Handler()};
+                Default_Handler();
             }
         )*
     };
 }
 
-stub_handlers!(
-BusFault_Handler, 
-MemManage_Handler, 
-UsageFault_Handler,
-SVCall_Handler,
-Debug_Monitor_Handler, 
-PendSV_Handler, 
-SysTick_Handler,
-WWDG_Handler, 
-PVD_Handler, 
-TAMP_STAMP_Handler, 
-RTC_WKUP_Handler,
-FLASH_Handler,
-RCC_Handler, 
-EXTI0_Handler, 
-EXTI1_Handler, 
-EXTI2_Handler, 
-EXTI3_Handler,
-EXTI4_Handler, 
-DMA1_Stream0_Handler, 
-DMA1_Stream1_Handler, 
-DMA1_Stream2_Handler,
-DMA1_Stream3_Handler, 
-DMA1_Stream4_Handler, 
-DMA1_Stream5_Handler, 
-DMA1_Stream6_Handler,
-ADC_Handler, 
-CAN1_TX_Handler, 
-CAN1_RX0_Handler, 
-CAN1_RX1_Handler, 
-CAN1_SCE_Handler,
-EXTI9_5_Handler, 
-TIM1_BRK_TIM9_Handler, 
-TIM1_UP_TIM10_Handler, 
-TIM1_TRG_COM_TIM11_Handler,
-TIM1_CC_Handler, 
-TIM2_Handler, 
-TIM3_Handler, 
-TIM4_Handler, 
-I2C1_EV_Handler,
-I2C1_ER_Handler, 
-I2C2_EV_Handler, 
-I2C2_ER_Handler, 
-SPI1_Handler, 
-SPI2_Handler,
-USART1_Handler, 
-USART2_Handler, 
-USART3_Handler, 
-EXTI15_10_Handler, 
-RTC_Alarm_Handler,
-OTG_FS_WKUP_Handler, 
-TIM8_BRK_TIM12_Handler, 
-TIM8_UP_TIM13_Handler, 
-TIM8_TRG_COM_TIM14_Handler,
-TIM8_CC_Handler, 
-DMA1_Stream7_Handler, 
-FSMC_Handler, 
-SDIO_Handler, 
-TIM5_Handler,
-SPI3_Handler, 
-UART4_Handler, 
-UART5_Handler, 
-TIM6_DAC_Handler, 
-TIM7_Handler,
-DMA2_Stream0_Handler, 
-DMA2_Stream1_Handler, 
-DMA2_Stream2_Handler, 
-DMA2_Stream3_Handler,
-DMA2_Stream4_Handler, 
-ETH_Handler, 
-ETH_WKUP_Handler, 
-CAN2_TX_Handler, 
-CAN2_RX0_Handler,
-CAN2_RX1_Handler, 
-CAN2_SCE_Handler, 
-OTG_FS_Handler, 
-DMA2_Stream5_Handler,
-DMA2_Stream6_Handler, 
-DMA2_Stream7_Handler, 
-USART6_Handler, 
-I2C3_EV_Handler,
-I2C3_ER_Handler, 
-OTG_HS_EP1_OUT_Handler, 
-OTG_HS_EP1_IN_Handler, 
-OTG_HS_WKUP_Handler,
-OTG_HS_Handler, 
-DCMI_Handler, 
-CRYP_Handler, 
-HASH_RNG_Handler, 
-FPU_Handler
-);
 
+stub_handlers!(
+        BusFault_Handler, 
+        MemManage_Handler, 
+        UsageFault_Handler, 
+        NMI_Handler, 
+        SVCall_Handler, 
+        Debug_Monitor_Handler, 
+        PendSV_Handler, 
+        SysTick_Handler,
+        WWDG_Handler, 
+        PVD_Handler, 
+        TAMP_STAMP_Handler, 
+        RTC_WKUP_Handler, 
+        FLASH_Handler,
+        RCC_Handler, 
+        EXTI0_Handler, 
+        EXTI1_Handler, 
+        EXTI2_Handler, 
+        EXTI3_Handler, 
+        EXTI4_Handler,
+        DMA1_Stream0_Handler, 
+        DMA1_Stream1_Handler, 
+        DMA1_Stream2_Handler, 
+        DMA1_Stream3_Handler,
+        DMA1_Stream4_Handler, 
+        DMA1_Stream5_Handler, 
+        DMA1_Stream6_Handler, 
+        ADC_Handler,
+        CAN1_TX_Handler, 
+        CAN1_RX0_Handler, 
+        CAN1_RX1_Handler, 
+        CAN1_SCE_Handler,
+        EXTI9_5_Handler, 
+        TIM1_BRK_TIM9_Handler, 
+        TIM1_UP_TIM10_Handler,
+        TIM1_TRG_COM_TIM11_Handler, 
+        TIM1_CC_Handler, 
+        TIM2_Handler, 
+        TIM3_Handler, 
+        TIM4_Handler,
+        I2C1_EV_Handler, 
+        I2C1_ER_Handler, 
+        I2C2_EV_Handler, 
+        I2C2_ER_Handler,
+        SPI1_Handler, 
+        SPI2_Handler, 
+        USART1_Handler, 
+        USART2_Handler, 
+        USART3_Handler,
+        EXTI15_10_Handler, 
+        RTC_Alarm_Handler, 
+        OTG_FS_WKUP_Handler, 
+        TIM8_BRK_TIM12_Handler,
+        TIM8_UP_TIM13_Handler, 
+        TIM8_TRG_COM_TIM14_Handler, 
+        TIM8_CC_Handler,
+        DMA1_Stream7_Handler, 
+        FSMC_Handler, 
+        SDIO_Handler, 
+        TIM5_Handler, 
+        SPI3_Handler,
+        UART4_Handler, 
+        UART5_Handler, 
+        TIM6_DAC_Handler, 
+        TIM7_Handler,
+        DMA2_Stream0_Handler, 
+        DMA2_Stream1_Handler, 
+        DMA2_Stream2_Handler,
+        DMA2_Stream3_Handler, 
+        DMA2_Stream4_Handler, 
+        ETH_Handler, 
+        ETH_WKUP_Handler,
+        CAN2_TX_Handler, 
+        CAN2_RX0_Handler, 
+        CAN2_RX1_Handler, 
+        CAN2_SCE_Handler,
+        OTG_FS_Handler, 
+        DMA2_Stream5_Handler, 
+        DMA2_Stream6_Handler, 
+        DMA2_Stream7_Handler,
+        USART6_Handler, 
+        I2C3_EV_Handler, 
+        I2C3_ER_Handler, 
+        OTG_HS_EP1_OUT_Handler,
+        OTG_HS_EP1_IN_Handler, 
+        OTG_HS_WKUP_Handler, 
+        OTG_HS_Handler,
+        DCMI_Handler, 
+        CRYP_Handler, 
+        HASH_RNG_Handler, 
+        FPU_Handler
+    );
+    
